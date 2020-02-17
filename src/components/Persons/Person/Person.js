489d1e57369e2a5029ac7f4c5a5c32682f95d5e1
Trai Nguyen <tmnguyen8@gmail.com>
2020-02-17 16:19:35 -0500

    Section 90

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,11 +3,12 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = ( props ) => {
-    const rnd = Math.random();
+    // const rnd = Math.random();
 
-    if ( rnd > 0.7 ) {
-        throw new Error( 'Something went wrong' );
-    }
+    // if ( rnd > 0.7 ) {
+    //     throw new Error( 'Something went wrong' );
+    // }
+    console.log('[Person.js] rendering')
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Person from "./Person/Person";
+
+const Persons = (props) => {
+    console.log('[Person.js] rendering...')
+    return props.persons.map( ( person, index ) => {
+    
+        return (
+        // <ErrorBoundary key={person.id}>
+        <Person
+            click={() => props.clicked( index )}
+            name={person.name}
+            age={person.age}
+            changed={( event ) => props.changed( event, person.id )} />
+        // </ ErrorBoundary>
+        )
+    })
+}
+
+export default Persons;
